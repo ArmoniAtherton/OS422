6dfc9dada4f34cd99b5351181ed4b3f5f87f2b6c
Armoni <athera@uw.edu>
2018-11-12 08:57:46 -0800

    change

--- prodcons.c
+++ prodcons.c
@@ -23,6 +23,8 @@
 int fill_ptr = 0;
 int use_ptr = 0;
 int count = 0;
+int produced = 0;
+int consumed = 0;
 
 // Define Locks and Condition variables here 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
@@ -38,6 +40,8 @@ int put(Matrix * value)
   *(bigmatrix + fill_ptr) = value;
   fill_ptr = (fill_ptr + 1) % MAX;
   count++;
+  printf("PUT, COUNT: %d\n", count);
+  produced++;
   return NULL;
 }
 
@@ -46,6 +50,8 @@ Matrix * get()
   Matrix * tmp = *( bigmatrix + use_ptr);
   use_ptr = (use_ptr + 1) % MAX;
   count--;
+  printf("GET, COUNT: %d\n", count);
+  consumed++;
   return tmp;
 }
 
@@ -54,7 +60,7 @@ void *prod_worker(void *arg)
   int i = 0;
   for (i = 0; i < LOOPS; i++) {
     pthread_mutex_lock(&lock);
-    while (count == MAX) {//SHOULD BE READY == MAX(PRODUCERS)
+    while (count == MAX) {
         pthread_cond_wait(&empty, &lock); 
     }
     put(GenMatrixRandom());
@@ -74,15 +80,57 @@ void *cons_worker(void *arg)
     while (count == 0) {
         pthread_cond_wait(&fill, &lock); 
     }
-    Matrix * tmp = get();
+    
+    Matrix * tmp1 = get();
+    // printf("THIS is Produced: %d, Consumed: %d, %d\n", produced, consumed, (produced - consumed)); 
+    while (tmp1 == NULL) {
+          FreeMatrix(tmp1);
+          tmp1 = get();
+    }
     pthread_cond_signal(&empty); 
     pthread_mutex_unlock(&lock); 
 
     //Print the matrix.
-    DisplayMatrix(tmp, stdout);
+    DisplayMatrix(tmp1, stdout);
+    FreeMatrix(tmp1);
     printf("\n");
   }
-  return NULL;
+  return NULL; 
 }
 
+
+    // int flag = 1;
+    // printf("THIS is Produced: %d, Consumed: %d, %d\n", produced, consumed, (produced - consumed)); 
+    // while (tmp2 == NULL && flag) {
+    //   if ((produced - consumed) < 1) {
+    //     printf("FLAG SET:\n");
+    //     flag = 0;
+    //   } else {
+    //     printf("IN HEREEEEE\n");
+    //     FreeMatrix(tmp2);
+    //     tmp2 = get();
+    //   }
+    // }
+    // printf("OUT OF LOOP");
+    // //This will check if we only have one matrix left in the bigmatrix.
+    // if (flag) {
+    //   printf("MULTIPLY: ");
+    //   Matrix * tmp3 = MatrixMultiply(tmp1, tmp2);
+    //   FreeMatrix(tmp1);
+    //   FreeMatrix(tmp2);
+    //   // DisplayMatrix(tmp3,stdout);
+    //   FreeMatrix(tmp3);
+    //   //Only on matrix left in bigmatrix
+    // } else {
+    //   printf("ONLY WHEN ONE LEFT: ");
+    //   DisplayMatrix(tmp1, stdout);
+    //   FreeMatrix(tmp1);
+    // }
+    // pthread_cond_signal(&empty); 
+    // pthread_mutex_unlock(&lock); 
+
+    // //Print the matrix.
+    // // DisplayMatrix(tmp1, stdout);
+    // // DisplayMatrix(tmp3, stdout);
+    // printf("\n");
 
